70d3032c3ee744e4b82715273a7bb1be33f58b43
DhanushikaTamilselvan <dhanushikatamil03@gmail.com>
2024-08-22 15:20:33 +0530

    Initial commit

---
+++ vite-project/src/About.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function About()
+{
+    return(
+    <>
+    <div className="about" >
+        <img className="background-image" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3_04QqNVVVD7CrO28HswyHkuy3LKIa565PQ&usqp=CAU" alt="web"></img>
+        <div className="overlay">
+    
+    <h1 className="h1">Dhanushika T</h1>
+    <h3 className="h3">Web Developer, UI Designer</h3>
+    </div></div>
+    </>
+    )
+}
+export default About
